ee0483d5e500f5e201a2bf22ed9b26a9072826f5
kavin-murugu <kavinmurugu25@gmail.com>
2025-07-21 18:06:36 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAfhmhPDQRguq5vyg4GlUlTd5s5PUwrbFw",
+  authDomain: "csda-aptitude.firebaseapp.com",
+  projectId: "csda-aptitude",
+  storageBucket: "csda-aptitude.firebasestorage.app",
+  messagingSenderId: "484239323097",
+  appId: "1:484239323097:web:5ecd5e941a761f647b73c8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
